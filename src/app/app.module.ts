0814be71bc3708789ca911e6740cacf8104d98c3
AncalagonKR <grahhuk@hotmail.com>
2021-04-29 19:02:15 -0500

    Initial Setup of Recipe App
    Basic layout and first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrganizerComponent } from './organizer/organizer.component';
+import { BookComponent } from './book/book.component';
+import { ListComponent } from './book/list/list.component';
+import { DetailsComponent } from './book/details/details.component';
+import { ItemsComponent } from './book/list/items/items.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrganizerComponent,
+    BookComponent,
+    ListComponent,
+    DetailsComponent,
+    ItemsComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
